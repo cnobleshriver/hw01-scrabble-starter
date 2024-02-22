6e4d9822c8d5246c629c1689d21371eb32ca08cf
cnobleshriver <100378265+cnobleshriver@users.noreply.github.com>
2024-02-22 13:21:17 -0500

    task 3 completed

--- src/scrabbleUtils.js
+++ src/scrabbleUtils.js
@@ -44,11 +44,42 @@ function canConstructWord(availableTiles, word) {
  */
 function baseScore(word) {
   // TASK #3: Base Score Calculator
-
-
-
-  // Replace the line below with your implementation.
-  throw new Error('Not implemented');
+  const pointsMap = {
+    '*': 0,
+    'e': 1,
+    'a': 1,
+    'i': 1,
+    'o': 1,
+    'n': 1,
+    'r': 1,
+    't': 1,
+    'l': 1,
+    's': 1,
+    'u': 1,
+    'd': 2,
+    'g': 2,
+    'b': 3,
+    'c': 3,
+    'm': 3,
+    'p': 3,
+    'f': 4,
+    'h': 4,
+    'v': 4,
+    'w': 4,
+    'y': 4,
+    'k': 5,
+    'j': 8,
+    'x': 8,
+    'q': 10,
+    'z': 10
+  };
+  let totalPoints = 0;
+  for(let char of word) {
+    if (char in pointsMap) {
+      totalPoints += pointsMap[char]
+    }
+  }
+  return totalPoints;
 }
 
 /**
